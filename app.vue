da20fcc503149edee2365f524772a1d38a70ae1d
Lavryniukk <leoyt109@gmail.com>
2024-01-04 00:57:34 +0200

    Added whatever those are

---
+++ app.vue
@@ -0,0 +1,57 @@
+<template>
+  <main
+    class="flex bg-primary-50 overflow-x-hidden w-screen h-fit flex-col items-center"
+  >
+    <section
+      class="flex items-center bg-primary-50 justify-center w-screen h-screen"
+    >
+      <div
+        :style="{ transform: `translateX(${-translate}px) skewX(-12deg)` }"
+        class="absolute left-1/3 h-full bg-gradient-to-b from-primary-400/10 to-primary-400/30 w-1/6"
+      ></div>
+      <div
+        :style="{ transform: `translateX(${translate / 2}px) skewX(12deg)` }"
+        class="absolute left-3/4 skew-x-12 h-full bg-gradient-to-b from-primary-400/10 to-primary-400/30 w-1/6"
+      />
+
+      <div></div>
+      <h1 class="font-extrabold font-sans text-9xl text-primary-950">
+        THE DEVELOPER
+      </h1>
+    </section>
+    <section
+      class="h-screen w-screen bg-gradient-to-t from-primary-100/50 to-primary-50"
+    >
+      <div
+        :style="{ transform: `translateX(${-translate}px) skewX(12deg)` }"
+        class="absolute left-[38.85%] skew-x-12 h-[200%] bg-primary-400/30 w-1/6"
+      />
+      <div
+        :style="{ transform: `translateX(${translate / 2}px) skewX(12deg)` }"
+        class="absolute left-[91.6%] skew-x-12 h-[200%] bg-primary-400/30 w-1/6"
+      />
+    </section>
+
+    <section class="w-screen h-screen bg-primary-200">
+      <div
+        :style="{ transform: `translateX(${-translate / 4}px) skewX(-12deg)` }"
+        class="absolute left-[110%] h-[100%] bg-primary-400/30 w-1/6"
+      />
+    </section>
+  </main>
+</template>
+<script setup>
+import { onMounted, onUnmounted } from "vue";
+let translate = ref(0);
+const handleScroll = () => {
+  translate.value = window.scrollY / 2;
+};
+
+onMounted(() => {
+  window.addEventListener("scroll", handleScroll);
+});
+
+onUnmounted(() => {
+  window.removeEventListener("scroll", handleScroll);
+});
+</script>
---
+++ tailwind.config.js
@@ -0,0 +1,27 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [],
+  theme: {
+    extend: {
+      colors: {
+        primary: {
+          50: "#f2fbf9",
+          100: "#d2f5ef",
+          200: "#a6e9e0",
+          300: "#71d7cd",
+          400: "#51c2ba",
+          500: "#2aa29c",
+          600: "#1f827e",
+          700: "#1d6866",
+          800: "#1b5453",
+          900: "#1b4645",
+          950: "#0a2829",
+        },
+      },
+      fontFamily: {
+        sans: ["Roboto"],
+      },
+    },
+  },
+  plugins: [],
+};
