6a7a7216d28bc2891650abfa9333dbd2ec675a19
Lavryniukk <leoyt109@gmail.com>
2024-01-04 13:35:31 +0200

    fix

--- app.vue
+++ app.vue
@@ -1,41 +1,43 @@
 <template>
   <main
-    class="flex bg-primary-50 overflow-x-hidden w-screen h-fit flex-col items-center"
+    class="flex flex-col items-center w-screen overflow-x-hidden bg-primary-50 h-fit"
   >
     <section
-      class="flex items-center bg-primary-50 justify-center w-screen h-screen"
+      class="flex items-center justify-center w-screen h-screen bg-primary-50"
     >
       <div
         :style="{ transform: `translateX(${-translate}px) skewX(-12deg)` }"
-        class="absolute left-1/3 h-full bg-gradient-to-b from-primary-400/10 to-primary-400/30 w-1/6"
+        class="absolute w-1/6 h-full pointer-events-none left-1/3 bg-gradient-to-b from-primary-400/10 to-primary-400/30"
       ></div>
       <div
         :style="{ transform: `translateX(${translate / 2}px) skewX(12deg)` }"
-        class="absolute left-3/4 skew-x-12 h-full bg-gradient-to-b from-primary-400/10 to-primary-400/30 w-1/6"
+        class="absolute w-1/6 h-[300vh] right-[20%] bg-gradient-to-b pointer-events-none from-primary-400/10 to-primary-400/30"
       />
 
       <div></div>
-      <h1 class="font-extrabold font-sans text-9xl text-primary-950">
+      <h1 class="font-sans font-extrabold text-9xl text-primary-950">
         THE DEVELOPER
       </h1>
     </section>
     <section
-      class="h-screen w-screen bg-gradient-to-t from-primary-100/50 to-primary-50"
+      class="w-screen h-screen bg-gradient-to-t from-primary-100/50 to-primary-50"
     >
       <div
         :style="{ transform: `translateX(${-translate}px) skewX(12deg)` }"
-        class="absolute left-[38.85%] skew-x-12 h-[200%] bg-primary-400/30 w-1/6"
-      />
-      <div
-        :style="{ transform: `translateX(${translate / 2}px) skewX(12deg)` }"
-        class="absolute left-[91.6%] skew-x-12 h-[200%] bg-primary-400/30 w-1/6"
+        class="absolute w-1/6 h-full skew-x-12 pointer-events-none left-1/3 bg-primary-400/30"
       />
     </section>
 
-    <section class="w-screen h-screen bg-primary-200">
+    <section
+      class="w-screen h-screen bg-gradient-to-b from-primary-100/50 to-primary-50"
+    >
+      <div
+        :style="{ transform: `translateX(${-translate}px) skewX(-12deg)` }"
+        class="absolute w-1/6 h-full pointer-events-none left-1/3 bg-gradient-to-b from-primary-400/30 to-primary-400/30"
+      ></div>
       <div
         :style="{ transform: `translateX(${-translate / 4}px) skewX(-12deg)` }"
-        class="absolute left-[110%] h-[100%] bg-primary-400/30 w-1/6"
+        class="absolute left-[110%] h-[100%] pointer-events-none bg-primary-400/30 w-1/6"
       />
     </section>
   </main>
